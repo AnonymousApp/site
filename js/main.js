c39c00fabeca214c823f1d5a3ebde2cf2c2c493a
blockchainz <37352972+blockchainz@users.noreply.github.com>
2019-02-23 21:07:25 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 jQuery(document).ready(function( $ ) {
 
-  // Header fixed and Back to top button
+  $(window).scroll(function () {
+    var height = $(window).height();
+    var scroll = $(window).scrollTop();
+    if (scroll) {
+      $(".header-hide").addClass("scroll-header");
+    } else {
+      $(".header-hide").removeClass("scroll-header");
+    }
+
+  });
+
+  // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
-      $('#header').addClass('header-fixed');
     } else {
       $('.back-to-top').fadeOut('slow');
-      $('#header').removeClass('header-fixed');
     }
   });
   $('.back-to-top').click(function(){
@@ -15,59 +24,65 @@ jQuery(document).ready(function( $ ) {
     return false;
   });
 
-  // Initiate the wowjs
+  // Initiate the wowjs animation library
   new WOW().init();
 
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
-    animation: {opacity:'show'},
+    animation: {
+      opacity: 'show'
+    },
     speed: 400
   });
 
   // Mobile Navigation
-  if( $('#nav-menu-container').length ) {
-    var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
-    $mobile_nav.find('> ul').attr({ 'class' : '', 'id' : '' });
-    $('body').append( $mobile_nav );
-    $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
-    $('body').append( '<div id="mobile-body-overly"></div>' );
+  if ($('#nav-menu-container').length) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({
+      id: 'mobile-nav'
+    });
+    $mobile_nav.find('> ul').attr({
+      'class': '',
+      'id': ''
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+    $('body').append('<div id="mobile-body-overly"></div>');
     $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
-    $(document).on('click', '.menu-has-children i', function(e){
+    $(document).on('click', '.menu-has-children i', function(e) {
       $(this).next().toggleClass('menu-item-active');
       $(this).nextAll('ul').eq(0).slideToggle();
       $(this).toggleClass("fa-chevron-up fa-chevron-down");
     });
 
-    $(document).on('click', '#mobile-nav-toggle', function(e){
+    $(document).on('click', '#mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
       $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
       $('#mobile-body-overly').toggle();
     });
 
-    $(document).click(function (e) {
+    $(document).click(function(e) {
       var container = $("#mobile-nav, #mobile-nav-toggle");
       if (!container.is(e.target) && container.has(e.target).length === 0) {
-       if ( $('body').hasClass('mobile-nav-active') ) {
+        if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
           $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
           $('#mobile-body-overly').fadeOut();
         }
       }
     });
-  } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
     $("#mobile-nav, #mobile-nav-toggle").hide();
   }
 
-  // Smoth scroll on page hash links
-  $('a[href*="#"]:not([href="#"])').on('click', function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
         var top_space = 0;
 
-        if( $('#header').length ) {
+        if ($('#header').length) {
           top_space = $('#header').outerHeight();
 
           if( ! $('#header').hasClass('header-fixed') ) {
@@ -79,12 +94,12 @@ jQuery(document).ready(function( $ ) {
           scrollTop: target.offset().top - top_space
         }, 1500, 'easeInOutExpo');
 
-        if ( $(this).parents('.nav-menu').length ) {
+        if ($(this).parents('.nav-menu').length) {
           $('.nav-menu .menu-active').removeClass('menu-active');
           $(this).closest('li').addClass('menu-active');
         }
 
-        if ( $('body').hasClass('mobile-nav-active') ) {
+        if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
           $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
           $('#mobile-body-overly').fadeOut();
@@ -94,47 +109,27 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Porfolio filter
-  $("#portfolio-flters li").click ( function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+  // Modal video
+  new ModalVideo('.js-modal-btn', {channel: 'youtube'});
 
-    var selectedFilter = $(this).data("filter");
-    $("#portfolio-wrapper").fadeTo(100, 0);
+  // Init Owl Carousel
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    autoplay: true,
+    loop: true,
+    margin: 30,
+    dots: true,
+    responsiveClass: true,
+    responsive: {
 
-    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
-
-    setTimeout(function() {
-      $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
-      $("#portfolio-wrapper").fadeTo(300, 1);
-    }, 300);
+      320: { items: 1},
+      480: { items: 2},
+      600: { items: 2},
+      767: { items: 3},
+      768: { items: 3},
+      992: { items: 4}
+    }
   });
-
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-
-  //Google Map
-  var get_latitude = $('#google-map').data('latitude');
-  var get_longitude = $('#google-map').data('longitude');
-
-  function initialize_google_map() {
-    var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-    var mapOptions = {
-      zoom: 14,
-      scrollwheel: false,
-      center: myLatlng
-    };
-    var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map
-    });
-  }
-  google.maps.event.addDomListener(window, 'load', initialize_google_map);
 
 // custom code
 
@@ -143,53 +138,121 @@ jQuery(document).ready(function( $ ) {
 
 
 
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+// typewriter
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2500;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
 
-//making the canvas full screen
-c.height = window.innerHeight;
-c.width = window.innerWidth;
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
 
-//chinese characters - taken from the unicode charset
-var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑0x0x0x0x0x0x0x0x0x0x0xx1234567890zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP";
-//converting the string into an array of single characters
-chinese = chinese.split("");
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
 
-var font_size = 9;
-var columns = c.width/font_size; //number of columns for the rain
-//an array of drops - one per column
-var drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-	drops[x] = 1;
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-//drawing the characters
-function draw()
-{
-	//Black BG for the canvas
-	//translucent BG to show trail
-	ctx.fillStyle = "rgba(243, 239, 255, 0.05)";
-	ctx.fillRect(0, 0, c.width, c.height);
+        var that = this;
+        var delta = 200 - Math.random() * 100;
 
-	ctx.fillStyle = "#927fd1"; //color - text
-	ctx.font = font_size + "px arial";
-	//looping over drops
-	for(var i = 0; i < drops.length; i++)
-	{
-		//a random chinese character to print
-		var text = chinese[Math.floor(Math.random()*chinese.length)];
-		//x = i*font_size, y = value of drops[i]*font_size
-		ctx.fillText(text, i*font_size, drops[i]*font_size);
+        if (this.isDeleting) { delta /= 2; }
 
-		//sending the drop back to the top randomly after it has crossed the screen
-		//adding a randomness to the reset to make the drops scattered on the Y axis
-		if(drops[i]*font_size > c.height && Math.random() > 0.975)
-			drops[i] = 0;
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
 
-		//incrementing Y coordinate
-		drops[i]++;
-	}
-}
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
 
-setInterval(draw, 33);
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
+
+
+
+    //matrix
+
+
+
+
+
+
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+
+        //making the canvas full screen
+        c.height = window.innerHeight;
+        c.width = window.innerWidth;
+
+        //chinese characters - taken from the unicode charset
+        var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑0x0x0x0x0x0x0x0x0x0x0xx1234567890zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP";
+        //converting the string into an array of single characters
+        chinese = chinese.split("");
+
+        var font_size = 9;
+        var columns = c.width/font_size; //number of columns for the rain
+        //an array of drops - one per column
+        var drops = [];
+        //x below is the x coordinate
+        //1 = y co-ordinate of the drop(same for every drop initially)
+        for(var x = 0; x < columns; x++)
+        	drops[x] = 1;
+
+        //drawing the characters
+        function draw()
+        {
+        	//Black BG for the canvas
+        	//translucent BG to show trail
+        	ctx.fillStyle = "rgba(243, 239, 255, 0.05)";
+        	ctx.fillRect(0, 0, c.width, c.height);
+
+        	ctx.fillStyle = "#927fd1"; //color - text
+        	ctx.font = font_size + "px arial";
+        	//looping over drops
+        	for(var i = 0; i < drops.length; i++)
+        	{
+        		//a random chinese character to print
+        		var text = chinese[Math.floor(Math.random()*chinese.length)];
+        		//x = i*font_size, y = value of drops[i]*font_size
+        		ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+        		//sending the drop back to the top randomly after it has crossed the screen
+        		//adding a randomness to the reset to make the drops scattered on the Y axis
+        		if(drops[i]*font_size > c.height && Math.random() > 0.975)
+        			drops[i] = 0;
+
+        		//incrementing Y coordinate
+        		drops[i]++;
+        	}
+        }
+
+        setInterval(draw, 33);
